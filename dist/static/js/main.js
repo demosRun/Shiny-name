283ce2ac50acdd221e1750495d4d5792a07baa42
puge <mail@puge.org>
2020-03-30 16:28:05 +0800

    save

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -8,16 +8,20 @@ document.body.addEventListener('touchmove', function (e) {
 }, {passive: false})
 
 // 重力感应
-window.addEventListener("deviceorientation", orientationHandler, false);  
-function orientationHandler(event) {
-  if (!sphere) return
-  sphere.rotation.y = -0.2 + event.gamma * 0.005
-  sphere.rotation.x = -1.6 + event.beta * 0.005
-  sphere.rotation.z = -0 + event.alpha * 0.001
-    // text = ""  
-    // var arrow = document.getElementById("arrow");  
-    // text += "左右旋转：rotate alpha{" + Math.round(event.alpha) + "deg)<br>";  
-    // text += "前后旋转：rotate beta{" + Math.round(event.beta) + "deg)<br>";  
-    // text += "扭转设备：rotate gamma{" + Math.round(event.gamma) + "deg)<br>";  
-    // arrow.innerHTML = text;  
-}
+// window.addEventListener("deviceorientation", orientationHandler, false);  
+// function orientationHandler(event) {
+//   if (!sphere) return
+//   sphere.rotation.y = -0.2 + event.gamma * 0.005
+//   sphere.rotation.x = -1.6 + event.beta * 0.005
+//   sphere.rotation.z = -0 + event.alpha * 0.001
+//     // text = ""  
+//     // var arrow = document.getElementById("arrow");  
+//     // text += "左右旋转：rotate alpha{" + Math.round(event.alpha) + "deg)<br>";  
+//     // text += "前后旋转：rotate beta{" + Math.round(event.beta) + "deg)<br>";  
+//     // text += "扭转设备：rotate gamma{" + Math.round(event.gamma) + "deg)<br>";  
+//     // arrow.innerHTML = text;  
+// }
+
+setTimeout(() => {
+  document.querySelector('.video-play video').style.top = -autoScaleInfo.hideHeight + 'px'
+}, 0)
